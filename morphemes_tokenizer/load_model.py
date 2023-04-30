80b036a0339964f111cc6e7885b705a6ebdd69c0
Wenxu <geminiwenxu@gmail.com>
2023-04-30 11:15:55 +0200

    convert replacement into function

--- morphemes_tokenizer/load_model.py
+++ morphemes_tokenizer/load_model.py
@@ -8,34 +8,44 @@ def consecutive(data, stepsize=1):
     return np.split(data, np.where(np.diff(data) != stepsize)[0] + 1)
 
 
-if __name__ == '__main__':
+def load_tokenizer():
     model_path = "pretrained-bert"
     tokenizer = BertTokenizer.from_pretrained(model_path)
-    sentence = "hello, deconstructed"
+    return tokenizer
+
+
+def replacement(sentence):
+    tokenizer = load_tokenizer()
     tokens = tokenizer.tokenize(sentence)
-    print(tokens)
+    print("tokens by pre-trained tokenizer: ", tokens)
     ls = []
     for idx, token in enumerate(tokens):
         if token.startswith("#"):
             ls.append(idx)
+    ls_array = consecutive(np.array(ls))
 
-    a = np.array(ls)
-    b = consecutive(a)
-
-    for i in b:
-
+    for i in ls_array:
         begin = i[0] - 1
         i = np.insert(i, 0, begin, axis=0)
-
         word = []
         for j in i:
             word.append(tokens[j])
         untokenized_word = "".join(word).replace("#", "")
-        print(untokenized_word)
-        meaning = find(untokenized_word)['prefix']['meaning'][0]
-        print(meaning)
+        print("untokenized word: ", untokenized_word)
+        prefix_meaning = find(untokenized_word)['prefix']['meaning'][0]
+        print("meaning of prefix: ", prefix_meaning)
+        suffix_meaning = find(untokenized_word)['suffix']['meaning'][0]
+        print("meaning of suffix: ", suffix_meaning)
 
-    replaced_sentence = sentence.replace(untokenized_word,meaning)
-    print(replaced_sentence)
-    new_tokens= tokens = tokenizer.tokenize(replaced_sentence)
-    print(new_tokens)
+    replaced_sentence = sentence.replace(untokenized_word, prefix_meaning + " " + suffix_meaning)
+    print("replaced sentence: ", replaced_sentence)
+    new_tokens = tokenizer.tokenize(replaced_sentence)
+    print("new tokens:", new_tokens)
+    encoding = tokenizer.encode_plus(new_tokens, add_special_tokens=True, truncation=True, padding="max_length",
+                                     return_attention_mask=True, return_tensors="pt")
+    print(encoding)
+
+
+if __name__ == '__main__':
+    sentence = "hello, deconstructed"
+    replacement(sentence)
