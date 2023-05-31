979ee92db400cdbe9f1011d22d54060b336d56d9
Wenxu <geminiwenxu@gmail.com>
2023-05-31 11:01:57 +0200

    replaced with selected meaning

--- morphemes_tokenizer/wenxu_tokenizer_mockup.py
+++ morphemes_tokenizer/wenxu_tokenizer_mockup.py
@@ -145,9 +145,9 @@ class WenxuTokenizer(GreedyTokenizer, PreTokenizer):
         """
         morphemes = []
         results, final_results = self.morphemes_finder(word)
-
         inflectional_affix = self.inflectional_finder(word)
         derivational_affix, derivational_strategy_affix = self.derivational_finder(word)
+        print("inflectional and derivational: ", inflectional_affix, "|", derivational_affix)
         inflectional = False
         derivational = False
         Not_found = False
@@ -165,52 +165,50 @@ class WenxuTokenizer(GreedyTokenizer, PreTokenizer):
                         inf_selected_form = form
                         inf_selected_strategy_affix = "suffix"
                         inflectional = True
-                        # print("inflectional selected form, meaning and strategy_affix: ", inf_selected_form,
-                        #       inf_selected_meaning,
-                        #       inf_selected_strategy_affix, inflectional)
+                        print("inflectional selected form, meaning and strategy_affix: ", inf_selected_form,
+                              inf_selected_meaning,
+                              inf_selected_strategy_affix, inflectional)
                     if form == derivational_affix:
                         de_selected_meaning = meaning
                         de_selected_form = form
                         de_selected_strategy_affix = derivational_strategy_affix
                         derivational = True
-                        # print("derivational selected form, meaning and strategy_affix: ", de_selected_form,
-                        #       de_selected_meaning,
-                        #       de_selected_strategy_affix, derivational)
-
+                        print("derivational selected form, meaning and strategy_affix: ", de_selected_form,
+                              de_selected_meaning,
+                              de_selected_strategy_affix, derivational)
                     if form != inflectional_affix and form != derivational_affix:
                         not_selected_meaning = meaning
                         not_selected_form = form
                         not_selected_strategy_affix = strategy
                         Not_found = True
-                        # print("None selected form, meaning and strategy_affix: ", not_selected_form,
-                        #       not_selected_meaning,
-                        #       not_selected_strategy_affix, Not_found)
-
+                        print("None selected form, meaning and strategy_affix: ", not_selected_form,
+                              not_selected_meaning,
+                              not_selected_strategy_affix, Not_found)
         if inflectional == True:
             selected_form = inf_selected_form
             selected_meaning = inf_selected_meaning
             selected_strategy_affix = inf_selected_strategy_affix
-
+            print("1", selected_meaning)
         elif derivational == True:
             selected_form = de_selected_form
             selected_meaning = de_selected_meaning
             selected_strategy_affix = de_selected_strategy_affix
-
+            print("2", selected_meaning)
         elif Not_found == True:
             selected_form = not_selected_form
             selected_meaning = not_selected_meaning
             selected_strategy_affix = not_selected_strategy_affix
-
+            print("3", selected_meaning)
         if selected_form != None:
             rest_word = word.replace(selected_form, '')
-
+            print(selected_form, rest_word)
             if selected_strategy_affix == "prefix":
-                morphemes.append(selected_form)
+                morphemes.append(selected_meaning)
                 morphemes.append(rest_word)
 
             elif selected_strategy_affix == "suffix":
                 morphemes.append(rest_word)
-                morphemes.append(selected_form)
+                morphemes.append(selected_meaning)
         else:
             morphemes.append(None)
         return morphemes
